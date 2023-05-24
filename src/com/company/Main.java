5f12b05844353b116e844120241bbd9220dd0543
Hamza <hamzamonther13@gmail.com>
2023-05-24 09:25:34 +0300

    Bruteforce method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,23 @@
 package com.company;
+import java.util.Scanner;
+
+
 
 public class Main {
+
+    public static int Power(int a, int b) {
+        int res = 1;
+        for (int i = 0; i < b; ++i) {
+            res *= a;
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
 	// write your code here
+        Scanner in = new Scanner (System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.println(Power(a, b));
     }
 }
